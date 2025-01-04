c1c0a8dbc6e4ae9d1d4bd5658c7357a27b407dee
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-01-04 21:33:08 +0300

    fix: conflict handling in application creation to prevent duplicate entries

--- src/modules/applications/applications.service.ts
+++ src/modules/applications/applications.service.ts
@@ -1,12 +1,25 @@
-import { Injectable } from "@nestjs/common"
-import { CreateApplicationDto } from "./dto/create-application.dto"
+import { ConflictException, Injectable } from "@nestjs/common"
 import { PrismaClient, PublishStatus } from "@prisma/client"
+import { CreateApplicationDto } from "./dto/create-application.dto"
 
 const prisma = new PrismaClient()
 
 @Injectable()
 export class ApplicationsService {
-  create(createApplicationDto: CreateApplicationDto, developerId: string) {
+  async create(
+    createApplicationDto: CreateApplicationDto,
+    developerId: string
+  ) {
+    const existingApplication = await prisma.application.findUnique({
+      where: {
+        reverseDomain: createApplicationDto.reverseDomain
+      }
+    })
+
+    if (existingApplication) {
+      throw new ConflictException("Application already exists")
+    }
+
     return prisma.application.create({
       data: {
         ...createApplicationDto,
