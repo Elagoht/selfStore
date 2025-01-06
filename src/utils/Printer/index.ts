3503f366bd775dca331b66c7b623dc38487e811c
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-01-06 20:09:51 +0300

    feat: Printer utility for colored console logging

---
+++ src/utils/Printer/index.ts
@@ -0,0 +1,37 @@
+class Printer {
+  public static info(message: string) {
+    this.print(message, "blue")
+  }
+
+  public static success(message: string) {
+    this.print(message, "green")
+  }
+
+  public static error(message: string) {
+    this.print(message, "red")
+  }
+
+  public static warn(message: string) {
+    this.print(message, "yellow")
+  }
+
+  public static debug(message: string) {
+    this.print(message, "magenta")
+  }
+
+  private static readonly colors = {
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    blue: "\x1b[34m",
+    magenta: "\x1b[35m",
+    reset: "\x1b[0m"
+  }
+
+  private static print(message: string, color: keyof typeof this.colors) {
+    // eslint-disable-next-line no-console
+    console.log(`${this.colors[color]}${message}${this.colors.reset}`)
+  }
+}
+
+export default Printer
