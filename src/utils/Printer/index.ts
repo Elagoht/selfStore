d62f4492346674e39e32a8bc6dea00a427448a73
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-01-10 00:02:52 +0300

    refactor: improve Printer utility methods for better logging and message formatting

--- src/utils/Printer/index.ts
+++ src/utils/Printer/index.ts
@@ -1,22 +1,29 @@
+/* eslint-disable no-console */
+
 class Printer {
   public static info(message: string) {
-    this.print(`[INFO]: ${message}`, "blue")
+    this.print("[INFO]: ", "blue")
+    console.log(message)
   }
 
   public static success(message: string) {
-    this.print(`[SUCCESS]: ${message}`, "green")
+    this.print("[SUCCESS]: ", "green")
+    console.log(message)
   }
 
   public static error(message: string) {
-    this.print(`[ERROR]: ${message}`, "red")
+    this.print("[ERROR]: ", "red")
+    console.error(message)
   }
 
   public static warn(message: string) {
-    this.print(`[WARN]: ${message}`, "yellow")
+    this.print("[WARN]: ", "yellow")
+    console.warn(message)
   }
 
-  public static debug(message: string) {
-    this.print(`[DEBUG]: ${message}`, "magenta")
+  public static debug(message: any) {
+    this.print("[DEBUG]: ", "cyan")
+    console.debug(message)
   }
 
   private static readonly colors = {
@@ -24,13 +31,16 @@ class Printer {
     green: "\x1b[32m",
     yellow: "\x1b[33m",
     blue: "\x1b[34m",
-    magenta: "\x1b[35m",
+    cyan: "\x1b[36m",
     reset: "\x1b[0m"
   }
 
   private static print(message: string, color: keyof typeof this.colors) {
-    // eslint-disable-next-line no-console
-    console.log(`${this.colors[color]}${message}${this.colors.reset}`)
+    process.stdout.write(
+      `${this.colors[color]}[${new Date().toISOString()}]-${
+        message
+      }${this.colors.reset}`
+    )
   }
 }
 
