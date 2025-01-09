44882c8d4693b02ac9cc8c585d47d1ba85802fd3
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-01-10 00:04:40 +0300

    refactor: clean up AppModule imports for better organization

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common"
 import { AdminModule } from "../admin/admin.module"
-import { DeveloperModule } from "../developer/developer.module"
 import { ApplicationsModule } from "../applications/applications.module"
+import { DeveloperModule } from "../developer/developer.module"
 
 @Module({
-  imports: [ApplicationsModule, AdminModule, DeveloperModule],
-  controllers: [],
-  providers: []
+  imports: [ApplicationsModule, AdminModule, DeveloperModule]
 })
 export class AppModule {}
