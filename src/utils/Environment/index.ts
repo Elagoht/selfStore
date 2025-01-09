459873cbbe7a7f193b61f4fe74448dc7897dbaa3
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-01-10 00:18:16 +0300

    feat: Environment utility class for configuration validation

---
+++ src/utils/Environment/index.ts
@@ -0,0 +1,23 @@
+class Environment {
+  public static readonly PORT = Number(process.env.PORT)
+  public static readonly JWT_SECRET = process.env.JWT_SECRET
+  public static readonly JWT_EXPIRATION_TIME = process.env.JWT_EXPIRATION_TIME
+  public static check() {
+    if (!Environment.PORT) throw new EnvironmentError("PORT is not set")
+    if (isNaN(Environment.PORT))
+      throw new EnvironmentError("PORT is not a number")
+    if (!Environment.JWT_SECRET)
+      throw new EnvironmentError("JWT_SECRET is not set")
+    if (!Environment.JWT_EXPIRATION_TIME)
+      throw new EnvironmentError("JWT_EXPIRATION_TIME is not set")
+  }
+}
+
+class EnvironmentError extends Error {
+  public constructor(message: string) {
+    super(message)
+    this.name = "EnvironmentError"
+  }
+}
+
+export default Environment
