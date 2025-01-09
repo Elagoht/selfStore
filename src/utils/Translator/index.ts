7b403beec7d9c8fde9e3f5dcc6a97753e41433e8
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-01-10 00:03:14 +0300

    feat: modifier support to translator

--- src/utils/Translator/index.ts
+++ src/utils/Translator/index.ts
@@ -3,7 +3,7 @@ import tr from "src/i18n/tr.json"
 
 declare global {
   // All dictionaries must have the same structure
-  type Dictionary = typeof en
+  type LangFileSchema = typeof en
 }
 
 import { parse } from "accept-language-parser"
@@ -20,25 +20,60 @@ class Translator {
   /**
    * Parse the period separated key and return the translated value
    *
-   * @param key - The key to translate
+   * @param pattern - The key + | + modifier=value
    * @returns The translated value
    * @example
    * ```ts
    * const translator = new Translator("en")
-   * const translated = translator.translate("common.errors.notFound")
+   * const translated = translator.translate("errors.notFound")
    * console.log(translated) // "Resource not found"
+   *
+   * const translated = translator.translate("errors.generics.conflict|entity=MyUserName")
+   * console.log(translated) // "MyUserName already exists"
    * ```
    */
-  public translate(key: string): string {
+  public translate(pattern: string): string {
     const dictionary = Translator.getDictionary(this.locale)
+    const [key, modifier] = pattern.split("|")
+
     const keys = key.split(".")
 
-    return keys.reduce((obj: any, key: string) => {
+    const message = keys.reduce((obj: any, key: string) => {
       if (obj && typeof obj === "object" && key in obj) {
         return obj[key]
       }
-      throw new Error(`Translation key "${key}" not found`)
+      return key
     }, dictionary)
+
+    return modifier ? Translator.modify(message, modifier) : message
+  }
+
+  private static modify(message: string, modifier: string): string {
+    const sanitizedModifiers = Translator.sanitizeModifiers(modifier)
+
+    let result = message
+    Object.entries(sanitizedModifiers).forEach(([key, value]) => {
+      const regex = new RegExp(`{{\\s*${key}\\s*}}`, "g")
+      result = result.replace(regex, value)
+    })
+
+    return result
+  }
+
+  /**
+   * Modifier string is a simple comma separated list of key=value pairs
+   * @param modifier - The modifier string
+   * @returns The sanitized modifier string
+   */
+  private static sanitizeModifiers(modifier: string): Record<string, string> {
+    return modifier.split(",").reduce(
+      (collection, pair) => {
+        const [key, value] = pair.split("=")
+        collection[key] = value
+        return collection
+      },
+      {} as Record<string, string>
+    )
   }
 
   private static determineLanguage(acceptedLanguage: string | null): Locale {
@@ -58,7 +93,7 @@ class Translator {
     return Translator.DICTIONARIES[locale]
   }
 
-  private static readonly DICTIONARIES: Record<Locale, Dictionary> = {
+  private static readonly DICTIONARIES: Record<Locale, LangFileSchema> = {
     en,
     tr
   }
