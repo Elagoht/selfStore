22935d08d6d1ff41b1f02b15f4eadb27a831b22e
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-01-10 00:18:36 +0300

    refactor(auth.config): replace hardcoded config with Environment

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -1,4 +1,6 @@
+import Environment from "src/utils/Environment"
+
 export const jwtConfig = {
-  secret: process.env.JWT_SECRET || "super-secret-key",
-  expiresIn: "1h"
+  secret: Environment.JWT_SECRET,
+  expiresIn: Environment.JWT_EXPIRATION_TIME
 }
